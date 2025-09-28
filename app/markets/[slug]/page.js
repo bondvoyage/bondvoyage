777183d4c0b18c5087582dc80f08894079b243dd
gy2335 <graceyang465@gmail.com>
2025-09-28 01:48:03 -0400

    test again

--- app/markets/[slug]/page.js
+++ app/markets/[slug]/page.js
@@ -1,18 +1,1 @@
-import { client } from "../lib/contentful";
 
-export default async function Market({ params }) {
-  const { slug } = params;
-  const entries = await client.getEntries({
-    content_type: "article",
-    "fields.category": slug,
-  });
-
-  return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold mb-4">{slug.toUpperCase()}</h1>
-      {entries.items.map((article) => (
-        <div key={article.sys.id}>{article.fields.title}</div>
-      ))}
-    </div>
-  );
-}
