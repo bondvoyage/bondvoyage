95537fbb99f20b99f4edf014996e43c8598b2e97
gy2335 <graceyang465@gmail.com>
2025-09-28 11:27:04 -0400

    test

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
 import "./globals.css";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <NavBar />
-        {children}
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
