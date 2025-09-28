64377b9caef22fc055f5a23ab8254dd93dc6fd42
gy2335 <graceyang465@gmail.com>
2025-09-28 11:16:54 -0400

    fixed layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
+        <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
